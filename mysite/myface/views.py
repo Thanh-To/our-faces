2a01e7bfcab949e056c8f42d47564c5f48eed754
Eddy Maric <emaric98@gmail.com>
2020-01-11 21:38:08 -0800

    Got script running on backend when button on frontend is pressed

--- mysite/myface/views.py
+++ mysite/myface/views.py
@@ -3,6 +3,13 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 import base64
 
+import numpy as np
+import cv2 as cv
+import matplotlib.pyplot as plt
+import os
+import sys
+import json
+
 def index(request):
     print("yeet711")
     context = {}
@@ -17,4 +24,44 @@ def testcall(request):
     with open(filename, 'wb') as f:
         f.write(imgdata)
 
-    return HttpResponse("You're a wizard Harry!")
+    scoreDict = {}
+
+    # Initiate SIFT detector
+    sift = cv.xfeatures2d.SIFT_create()
+
+    inputImg = cv.imread('webScreenshot.jpg',cv.IMREAD_GRAYSCALE)
+    comparisonImagesPath = 'averages/'
+
+    for subdir, dirs, files in os.walk(comparisonImagesPath):
+        for file in files:
+
+            comparisonImage = cv.imread(os.path.join(subdir,file),cv.IMREAD_GRAYSCALE) # trainImage
+
+            print("Comparing input image to " + file)
+
+            # find the keypoints and descriptors with SIFT
+            kp1, des1 = sift.detectAndCompute(inputImg,None)
+            kp2, des2 = sift.detectAndCompute(comparisonImage,None)
+
+            # FLANN parameters
+            FLANN_INDEX_KDTREE = 1
+            index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+            search_params = dict(checks=50)   # or pass empty dictionary
+            flann = cv.FlannBasedMatcher(index_params,search_params)
+            matches = flann.knnMatch(des1,des2,k=2)
+
+            # Need to draw only good matches, so create a mask
+
+            goodMatchCounter = 0
+
+            # ratio test as per Lowe's paper
+            for i,(m,n) in enumerate(matches):
+                if m.distance < 0.7*n.distance:
+                    goodMatchCounter += 1
+
+            faculty = file.split('_')[-1].split('.')[0]
+            scoreDict[faculty] = goodMatchCounter
+
+    return HttpResponse(json.dumps(scoreDict, indent = 4))
+
+    
